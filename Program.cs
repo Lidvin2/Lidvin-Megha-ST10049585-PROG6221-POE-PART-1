791b1b804bc2db6cb5d70f53d5d79d4f8189775e
Lidvin2 <130862579+Lidvin2@users.noreply.github.com>
2024-04-19 13:31:13 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PROG6221_POEPART1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Recipe recipe = new Recipe();
+            while (true) 
+            {
+                Console.WriteLine("1. Enter ingredients");
+                Console.WriteLine("2. Enter steps");
+                Console.WriteLine("3. Display recipe");
+                Console.WriteLine("4. Scale recipe");
+                Console.WriteLine("5. Reset quantities");
+                Console.WriteLine("6. Clear recipe");
+                Console.WriteLine("7. Exit");
+                Console.WriteLine();
+
+                Console.Write("Enter your choice: ");
+                int choice = int.Parse(Console.ReadLine());
+                Console.WriteLine();
+
+                switch (choice) 
+                {
+                    case 1:
+                        recipe.AddIngredients();
+                        break;
+
+                    case 2:
+                        recipe.AddSteps();
+                        break;
+
+                    case 3:
+                        recipe.DisplayRecipe();
+                        break;
+
+                    case 4:
+                        Console.WriteLine("Enter the scaling factor (0.5, 2, or 3): ");
+                        double factor = double.Parse(Console.ReadLine());
+                        recipe.ScaleRecipe(factor);
+                        break;
+
+                    case 5:
+                        recipe.ResetQuantities();
+                        break;
+
+                    case 6:
+                        recipe.ClearData();
+                        break;
+
+                    case 7:
+                        Environment.Exit(0);
+                        break;
+
+                    default:
+
+                        Console.WriteLine("Invalid choice. Please try again.");
+
+                        break;
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ Recipe.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PROG6221_POEPART1
+{
+    internal class Recipe
+    {
+        private string[] ingredients;
+        private double[] quantities;
+        private string[] units;
+        private string[] steps;
+        private int numIngredients;
+        private int numSteps;
+        private double scale;
+
+        public Recipe() 
+        {
+            ingredients = new string[0];
+            quantities = new double[0];
+            units = new string[0];
+            steps = new string[0];
+            numIngredients = 0;
+            numSteps = 0;
+            scale = 1.0;
+        }
+
+        public void AddIngredients()
+        {
+            Console.WriteLine("Enter the number of ingredient:");
+            numIngredients =int.Parse(Console.ReadLine());
+
+            ingredients = new string[numIngredients];
+            quantities = new double[numIngredients];
+            units = new string[numIngredients];
+
+            for (int i = 0; i < numIngredients; i++)
+            {
+                Console.WriteLine("Name" + (i + 1) + ":");
+                ingredients[i] = Console.ReadLine();
+                Console.WriteLine("Quantity" + (i + 1) + ":");
+                quantities[i] = double.Parse(Console.ReadLine());
+                Console.WriteLine("Unit of measurement" + (i + 1) + ":");
+                units[i] = Console.ReadLine();
+            }
+        }
+
+        public void AddSteps() 
+        {
+            Console.WriteLine("Enter the number of step:");
+            numSteps = int.Parse(Console.ReadLine());
+
+            steps = new string[numSteps];
+
+            for (int i = 0;i < numSteps;i++) 
+            {
+                Console.WriteLine("Description" + (i + 1) + ":");
+                steps[i] = Console.ReadLine();
+            }
+        }
+
+        public void DisplayRecipe()
+        {
+            Console.WriteLine("Recipe:");
+            Console.WriteLine();
+            for (int i = 0; i < numIngredients; i++) 
+            {
+                Console.WriteLine(quantities[i] * scale + " " + units[i] + "" + ingredients[i]);
+            }
+            Console.WriteLine();
+            for (int i = 0; i < numSteps; i++) 
+            {
+                Console.WriteLine((i + 1) + "." + steps[i]);
+            }
+        }
+
+        public void ScaleRecipe(double factor) 
+        {
+            scale = factor;
+        }
+
+        public void ResetQuantities() 
+        {
+            scale = 1.0;
+        }
+
+        public void ClearData()
+        {
+            ingredients = new string[0];
+
+            quantities = new double[0];
+
+            units = new string[0];
+
+            steps = new string[0];
+
+            numIngredients = 0;
+
+            numSteps = 0;
+
+            scale = 1.0;
+        }
+
+        
+    }
+}
